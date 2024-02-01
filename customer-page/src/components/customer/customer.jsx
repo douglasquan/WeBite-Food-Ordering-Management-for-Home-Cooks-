81d1ea704f4e100bbf46bfbc7c976e26555b159f
Weiming Quan <weiming.quan@mail.utoronto.ca>
2024-02-01 12:11:08 -0500

    commit starting code for customer

---
+++ customer-page/src/components/customer/customer.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+
+import './customer.css';
+
+function Customer() {
+    return (
+      <div>
+        
+      </div>
+    );
+    
+}
+  
+export default Customer;
