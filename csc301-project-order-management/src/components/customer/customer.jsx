75a30ac60e2ae4d6e9b008567552e2f77d4f924f
diifault <mails.to.nano@gmail.com>
2024-02-05 16:30:58 -0500

    Setting up home page and NavBar

---
+++ csc301-project-order-management/src/components/customer/customer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const customer = () => {
+    return (
+        <div>
+            <h1>TEMPORARY CUSTOMER PAGE</h1>
+        </div>
+    );
+};
+ 
+export default customer;
