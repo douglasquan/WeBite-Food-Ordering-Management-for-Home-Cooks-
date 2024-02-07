32af73b344c48bbb86ada658b2e2d668041ee3a8
Kevin ni <k.ni@mail.utoronto.ca>
2024-02-07 17:51:34 -0500

    fixed gateway bugs involving invalid return types

--- backend/gateway.py
+++ backend/gateway.py
@@ -4,61 +4,75 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/chef', methods=['POST', 'GET', 'DELETE'])
+@app.route('/chef', methods=['POST', 'GET', 'DELETE', 'PUT'])
 def chef():
     if request.method == 'GET':
         r = request.query_string.decode()
         new_url = routes['chef'] + f'?{r}'
         response = requests.get(new_url)
-        return response.status_code, response.json()
+        return response.json(), response.status_code
     elif request.method == 'POST':
         r = request.get_json()
         response = requests.post(routes['chef'], json=r)
-        return response.status_code, response.json()
+        return response.json(), response.status_code
     elif request.method == 'DELETE':
         r = request.get_json()
         response = requests.delete(routes['chef'], json=r)
-        return response.status_code, response.json()
+        return response.json(), response.status_code
+    elif request.method == 'PUT':
+        r = request.get_json()
+        response = requests.put(routes['chef'], json=r)
+        return response.json(), response.status_code
     else:
-        return 400, "Bad Request"
+        return "Bad Request", 400
 
 
-@app.route('/order', methods=['POST', 'GET', 'DELETE'])
+@app.route('/order', methods=['POST', 'GET', 'DELETE', 'PUT'])
 def order():
     if request.method == 'GET':
         r = request.query_string.decode()
         new_url = routes['orders'] + f'?{r}'
         response = requests.get(new_url)
-        return response.status_code, response.json()
+        return response.json(), response.status_code
     elif request.method == 'POST':
         r = request.get_json()
         response = requests.post(routes['orders'], json=r)
-        return response.status_code, response.json()
+        return response.json(), response.status_code
     elif request.method == 'DELETE':
         r = request.get_json()
         response = requests.delete(routes['orders'], json=r)
-        return response.status_code, response.json()
+        return response.json(), response.status_code
+    elif request.method == 'PUT':
+        r = request.get_json()
+        response = requests.put(routes['chef'], json=r)
+        return response.json(), response.status_code
     else:
-        return 400,"Bad Request"
+        return "Bad Request", 400
 
 
-@app.route('/user', methods=['POST', 'GET', 'DELETE'])
+@app.route('/customer', methods=['POST', 'GET', 'DELETE', 'PUT'])
 def user():
     if request.method == 'GET':
         r = request.query_string.decode()
-        new_url = routes['user'] + f'?{r}'
+        new_url = routes['customer'] + f'?{r}'
         response = requests.get(new_url)
-        return response.status_code, response.json()
+        return response.json(), response.status_code
     elif request.method == 'POST':
         r = request.get_json()
-        response = requests.post(routes['user'], json=r)
-        return response.status_code, response.json()
+        print(request.endpoint)
+        print(request.url)
+        response = requests.post(routes['customer'], json=r)
+        return response.json(), response.status_code
     elif request.method == 'DELETE':
         r = request.get_json()
-        response = requests.delete(routes['user'], json=r)
-        return response.status_code, response.json()
+        response = requests.delete(routes['customer'], json=r)
+        return response.json(), response.status_code
+    elif request.method == 'PUT':
+        r = request.get_json()
+        response = requests.put(routes['customer'], json=r)
+        return response.json(), response.status_code
     else:
-        return 400,"Bad Request"
+        return "Bad Request", 400
 
 
 if __name__ == "__main__":
@@ -81,8 +95,8 @@ if __name__ == "__main__":
         print("Config file missing services")
         exit(1)
     routes = {
-        "user": f"http://{customer_ip}:{customer_port}/customer",
-        "chef": f"http://{chef_port}:{chef_ip}/chef",
-        "orders": f"http://{orders_port}:{orders_ip}/orders"
+        "customer": f"http://{customer_ip}:{customer_port}/customer",
+        "chef": f"http://{chef_ip}:{chef_port}/chef",
+        "orders": f"http://{orders_ip}:{orders_port}/order"
     }
     app.run(port=config_data['Gateway']['port'], host=config_data['Gateway']['ip'], debug=True)
