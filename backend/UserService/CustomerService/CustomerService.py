4c2bc0f2efba170c5b86a70b947f5436f8b82ca1
Kevin <kevin.kni.ni@gmail.com>
2024-02-23 18:31:45 -0500

    Created UserService, updated chef and customer post and get services to work with the new /user endpoint.

--- backend/UserService/CustomerService/CustomerService.py
+++ backend/UserService/CustomerService/CustomerService.py
@@ -5,11 +5,12 @@ import json
 import sys
 import os
 
-
 app = Flask(__name__)
 
-DATABASE = 'customers.db'
-#meal_DATABASE = 'meals.db'
+DATABASE = '../users.db'
+
+
+# meal_DATABASE = 'meals.db'
 
 
 def create_tables():
@@ -29,20 +30,20 @@ def create_tables():
     conn_cus = get_db_connection('customers.db')
     cursor_cus = conn_cus.cursor()
     cursor_cus.execute('''DROP TABLE IF EXISTS customers;''')
-    #conn_meal = get_db_connection('meals.db')
-    #cursor_meal = conn_meal.cursor()
+    # conn_meal = get_db_connection('meals.db')
+    # cursor_meal = conn_meal.cursor()
     cursor_cus.execute(tables[0])
-    #cursor_meal.execute(tables[1])
+    # cursor_meal.execute(tables[1])
 
     conn_cus.commit()
     conn_cus.close()
-    #conn_meal.commit()
-    #conn_meal.close()
+    # conn_meal.commit()
+    # conn_meal.close()
 
 
-#@app.cli.command('init_db')
-def initialize_database():
-    create_tables()
+# @app.cli.command('init_db')
+# def initialize_database():
+#     create_tables()
 
 
 def get_db_connection(db_name):
@@ -64,33 +65,34 @@ def post_handler():
     else:
         return jsonify({"message": "Command's wrong"}), 409
 
+
 def create_customer():
     customer_data = request.json
-    #print(request.json)
-    #id = customer_data.get('id')
+    # print(request.json)
+    # id = customer_data.get('id')
     name = customer_data.get('name')
     email = customer_data.get('email')
     password = customer_data.get('password_hash')
-    #print(name, email, password)
+    # print(name, email, password)
 
     if not (name and email and password):
-        #print("no")
+        # print("no")
         return jsonify({"message": "Missing fields"}), 400
     password_hash = generate_password_hash(password)
-    #print("password_hash")
+    # print("password_hash")
     try:
-        #print("3")
-        conn = get_db_connection('customers.db')
+        # print("3")
+        conn = get_db_connection('users.db')
         cur = conn.cursor()
-        #print("4")
+        # print("4")
         # sql = """INSERT INTO customers (name, email, password_hash) VALUES (?, ?, ?)"""
         # cur = cur.execute(sql, (name, email, password_hash))
         cur.execute('INSERT INTO customers (name, email, password_hash) VALUES (?, ?, ?)',
-                     (name, email, password_hash))
-        #print("5")
+                    (name, email, password_hash))
+        # print("5")
         conn.commit()
         id = cur.lastrowid
-        #print("id", id)
+        # print("id", id)
     except sqlite3.IntegrityError:
         return jsonify({"message": "Email already exists"}), 409
     finally:
@@ -99,7 +101,7 @@ def create_customer():
                 "name": name,
                 "email": email,
                 "password_hash": password_hash}
-    #print(response)
+    # print(response)
     return response, 200
 
 
@@ -127,7 +129,8 @@ def delete_customer():
     if customer is None:
         conn.close()
         return jsonify({"message": "Customer not found"}), 404
-    if name != customer['name'] or email != customer['email'] or not check_password_hash(customer['password_hash'], password):
+    if name != customer['name'] or email != customer['email'] or not check_password_hash(customer['password_hash'],
+                                                                                         password):
         conn.close()
         return jsonify({"message": "Authentication failed"}), 409
     conn.execute('DELETE FROM customers WHERE id = ?', (customer_id,))
@@ -135,58 +138,15 @@ def delete_customer():
     conn.close()
     return jsonify({"message": "Customer deleted successfully"}), 200
 
-
-# @app.route('/meal/create', methods=['POST'])
-# def create_meal():
-#     meal_data = request.json
-#     name = meal_data.get('name')
-#     cost = meal_data.get('cost')
-#
-#     if not name or cost is None:
-#         return jsonify({"message": "Name and cost are required"}), 400
-#
-#     conn = get_db_connection('meals.db')
-#     conn.execute('INSERT INTO meals (name, cost) VALUES (?, ?)', (name, cost))
-#     conn.commit()
-#     conn.close()
-#
-#     return jsonify({"message": "Meal added successfully"}), 201
-
-
-# @app.route('/meal/<meal_name>', methods=['GET'])
-# def get_meal_cost(meal_name):
-#     conn = get_db_connection('customers.db')
-#     meal = conn.execute('SELECT * FROM meals WHERE name = ?',
-#                         (meal_name,)).fetchone()
-#     conn.close()
-#     if meal is None:
-#         return jsonify({"message": "Meal not found"}), 404
-#     return jsonify({"name": meal["name"], "cost": meal["cost"]}), 200
-
-
-# @app.route('/meal/<int:meal_id>', methods=['DELETE'])
-# def delete_meal(meal_id):
-#     conn = get_db_connection('meals.db')
-#     meal = conn.execute('SELECT * FROM meals WHERE id = ?', (meal_id,)).fetchone()
-#     if meal is None:
-#         conn.close()
-#         return jsonify({"message": "Meal not found"}), 404
-#
-#     conn.execute('DELETE FROM meals WHERE id = ?', (meal_id,))
-#     conn.commit()
-#     conn.close()
-#     return jsonify({"message": "Meal deleted successfully"}), 200
-
-
-#@app.route('/customer/login', methods=['POST'])
+# @app.route('/customer/login', methods=['POST'])
 def login_customer():
-    #credentials = request.json
+    # credentials = request.json
     try:
-        #print(request.json)
+        # print(request.json)
         email = request.args.get('email')
         print(email)
         password = request.args.get('password_hash')
-        #print(password)
+        # print(password)
     except KeyError:
         return "bad request", 400
     # email = credentials.get('email')
@@ -202,13 +162,12 @@ def login_customer():
 
     if customer and check_password_hash(customer['password_hash'], password):
         # Successfully authenticated
-        #session['customer_id'] = customer['id']  # Store customer ID in session for future requests, maybe useful if we
+        # session['customer_id'] = customer['id']  # Store customer ID in session for future requests, maybe useful if we
         # want a logout feature
         return jsonify({"message": "Login successful"}), 200
     else:
         # Authentication failed
         return jsonify({"message": "Invalid email or password"}), 401
-
 
 
 if __name__ == '__main__':
@@ -221,9 +180,10 @@ if __name__ == '__main__':
     # port = config['CustomerSerive']['port']
     # port = 8001
     # app.run(debug=True, port=port)
-    initialize_database()
+    # initialize_database()
     current_dir = os.getcwd()
-    config_path = os.path.abspath(os.path.join(os.path.join(os.path.join(current_dir, os.pardir), os.pardir), "config.json"))
+    config_path = os.path.abspath(
+        os.path.join(os.path.join(current_dir, os.pardir), "config.json"))
     with open(config_path, 'r') as config_file:
         config_data = json.load(config_file)
     # getting ip for everything
